f035c4d5b8a77ae8c217c45bdcca214c3039c035
Momen Alany <momen.sameer940@gmail.com>
2021-11-01 00:04:11 +0300

    reconfigured the desktop installation

--- moarch-applications.sh
+++ moarch-applications.sh
@@ -28,11 +28,24 @@ echo "Done!"
 echo
 
 
+DESKTOP=(
+  'lightdm'                # DM
+  'lightdm-gtk-greeter'    # DM greeter
+  'plasma'                 # KDE DE
+  'kde-system-meta'        # KDE system apps
+  'kde-accessibility-meta' # KDE accessability apps
+  'kde-games-meta'         # KDE games
+)
+
+
 echo
 echo "INSTALLING DESKTOP"
 echo
 
-sudo pacman -S lightdm xorg-server lightdm-gtk-greeter plasma kde-system-meta kde-accessibility-meta kde-games-meta
+for APP in "${DESKTOP[@]}"; do
+    echo "INSTALLING: ${APP}"
+    sudo pacman -S --asdeps --noconfirm --needed $(pacman -Si $APP | sed -n '/^Opt/,/^Conf/p' | sed '$d' | sed 's/^Opt.*://g' | sed 's/^\s*//g' | tr '\n' ' ')
+done
 sudo systemctl enable lightdm
 
 echo 
@@ -147,7 +160,7 @@ PKGS=(
 
 for PKG in "${PKGS[@]}"; do
     echo "INSTALLING: ${PKG}"
-    sudo pacman -S "$PKG" --noconfirm --needed
+    sudo pacman -S --asdeps --noconfirm --needed $(pacman -Si $PKG | sed -n '/^Opt/,/^Conf/p' | sed '$d' | sed 's/^Opt.*://g' | sed 's/^\s*//g' | tr '\n' ' ')
 done
 
 echo
