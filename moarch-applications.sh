a4fc9e2e6184bb51df18f9b23e8a4c655b23e0ad
Momen Alany <41761574+Aldazdar123@users.noreply.github.com>
2021-10-31 20:36:03 +0300

    Update moarch-applications.sh
    added xorg installation

--- moarch-applications.sh
+++ moarch-applications.sh
@@ -6,6 +6,29 @@
 # |_|  |_|\___/|_|  |_|_____|_| \_| |____/   \___/|____/
 
 echo
+echo "INSTALLING XORG"
+echo
+
+PKGS=(
+        'xorg-server'           # XOrg server
+        'xorg-apps'             # XOrg apps group
+        'xorg-xinit'            # XOrg init
+        'xf86-video-intel'      # 2D/3D video driver
+        'mesa'                  # Open source version of OpenGL
+        'xf86-input-libinput'   # Trackpad driver for Dell XPS
+)
+
+for PKG in "${PKGS[@]}"; do
+    echo "INSTALLING: ${PKG}"
+    sudo pacman -S "$PKG" --noconfirm --needed
+done
+
+echo
+echo "Done!"
+echo
+
+
+echo
 echo "INSTALLING DESKTOP"
 echo
 
