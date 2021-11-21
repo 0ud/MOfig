eff6b3da0a6d0b82e832661de3dbff04433d70f7
Momen Alany <momen.sameer940@gmail.com>
2021-11-21 19:56:26 +0300

    rewrote how the script should work

---
+++ 3-moarch-applications.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# |  \/  |/ _ \( ) ___|   / _ \/ ___|
+# | |\/| | | | |/\___ \  | | | \___ \
+# | |  | | |_| |  ___) | | |_| |___) |
+# |_|  |_|\___/  |____/   \___/|____/
+
+echo
+echo "INSTALLING SOFTWARE"
+echo
+
+
+paru -S --noconfirm --needed - < ./pkgs.txt
+
+echo
+echo "Done!"
+echo
